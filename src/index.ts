44971bdd385295a1ccc25a50c830a97798ee75c4
sashman <sashman90@gmail.com>
2025-03-24 08:12:14 +0000

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,65 @@
 'use strict';
 
-import { Logger } from 'sitka';
+import { Warhammer40kList } from './list';
 
-export class Example {
-	/* Private Instance Fields */
+function parseGWList(list_string: string): Warhammer40kList {
 
-	private _logger: Logger;
+	// valid GW list must start with 4 non blank lines and contrain at least one "CHARACTERS" "BATTLELINE" "OTHER DATASHEETS" "ALLIED UNITS" lines
+	// use regex to check the list format
+	// if the list is not valid return an error
+	// if the list is valid parse the list
 
-	/* Constructor */
-
-	constructor() {
-		this._logger = Logger.getLogger({ name: this.constructor.name });
+	if (!list_string || list_string.length === 0) {
+		throw new Error('Invalid GW list string');
 	}
 
+	// check for at least one of each of the following lines
+	const valid_list = /CHARACTERS|BATTLELINE|OTHER DATASHEETS|ALLIED UNITS/;
+	if (!valid_list.test(list_string)) {
+		throw new Error('Invalid GW list string');
+	}
+	
+	// split the list into lines and remove empty lines
+	const lines = list_string.split('\n').filter((line) => line.trim().length > 0);
+	const listName = lines[0].trim();
+	const listFaction = lines[1].trim();
+	const listDetachment = lines[2].trim();
+	// use regex to extract the battle size from the 4th line, everything before the first '('
+	const battleSize = lines[3].match(/.*(?=\()/);
+	if (!battleSize) {
+		throw new Error('Invalid GW list string');
+	}
+
+	const listBattleSize = battleSize[0].trim();
+	// use regex to extract the points from the 3rd line
+	const points = lines[3].match(/\d+/);
+	const listPoints = points ? parseInt(points[0]) : 0;
+
+	// parse the list in an event driven way, an event could be a start or end of a unit, a model, a weapon
+	// a unit always starts with a line that ends with 'Points)'
+	// repalce all • with a single whitespace, do not trim the lines
+	// make sure to rely on whitespace to determine the start of models or weapons
+	// if a line starts with 4 spaces it can be a model of a unit or a weapon
+	// if a line starts with 6 spaces it is a weapon
+	// if a line that looks like a model has no weapons it is a weapon
+	// all models must have at least one weapon
+	// if a unit has not models create a model that replicates the unit
+
+	
+	return {
+		name: listName,
+		faction: listFaction,
+		detachment: { name: listDetachment },
+		battleSize: listBattleSize,
+		points: listPoints,
+		units: units
+	};
+}
+
+export class ListParser {
 	/* Public Instance Methods */
 
-	public exampleMethod(param: string): string {
-		this._logger.debug('Received: ' + param);
-		return param;
+	public parse(list_string: string): Warhammer40kList {		
+		return parseGWList(list_string);
 	}
 }
--- test/index.ts
+++ test/index.ts
@@ -1,17 +1,73 @@
 'use strict';
 
 import { expect } from 'chai';
-import { Example } from '../dist/index';
+import { ListParser } from '../src/index';
 
-describe('Example class', () => {
-	it('should create an instance using its constructor', () => {
-		const example: Example = new Example();
-		expect(example, 'example should exist').to.exist;
+describe('Basic list parsing', () => {
+	it('throw an error if the list is empty string', () => {
+		const list = new ListParser();
+		expect(() => list.parse('')).to.throw('Invalid GW list string');
 	});
-	it('should return whatever is passed to exampleMethod()', () => {
-		const example: Example = new Example();
-		const param = 'This is my param.';
-		const returnValue: string = example.exampleMethod(param);
-		expect(returnValue).to.equal(param, 'returns the value passed as a parameter');
+
+	it('throw an error if list string does not have at least one "CHARACTERS" "BATTLELINE" "OTHER DATASHEETS" "ALLIED UNITS" lines', () => {
+		const list = new ListParser();
+		expect(() => list.parse('   ')).to.throw('Invalid GW list string');
 	});
+
+	it('parse a dummy valid list with no units', () => {
+		const dummyList = `
+Folger Other Clone
+
+Adeptus Custodes
+Solar Spearhead
+Strike Force (2000 Points)
+
+CHARACTERS
+		`
+		const list = new ListParser();
+		const parsedList = list.parse(dummyList);
+		expect(parsedList.name).to.equal('Folger Other Clone');
+		expect(parsedList.faction).to.equal('Adeptus Custodes');
+		expect(parsedList.detachment.name).to.equal('Solar Spearhead');
+		expect(parsedList.points).to.equal(2000);
+		expect(parsedList.battleSize).to.equal('Strike Force');
+		expect(parsedList.units).to.be.empty;
+	});
+
+	it('parse a dummy valid list with one unit', () => {
+		const dummyList = `
+Folger Other Clone
+
+Adeptus Custodes
+Solar Spearhead
+Strike Force (2000 Points)
+
+CHARACTERS
+
+Blade Champion (145 Points)
+  • 1x Vaultswords
+  • Enhancements: Adamantine Talisman
+`
+		const list = new ListParser();
+		const parsedList = list.parse(dummyList);
+		expect(parsedList.name).to.equal('Folger Other Clone', 'List name');
+		expect(parsedList.faction).to.equal('Adeptus Custodes', 'Faction');
+		expect(parsedList.detachment.name).to.equal('Solar Spearhead', 'Detachment');
+		expect(parsedList.points).to.equal(2000, 'Points');
+		expect(parsedList.battleSize).to.equal('Strike Force', 'Battle size');
+		expect(parsedList.units).to.have.lengthOf(1, 'Units');
+		expect(parsedList.units[0].name).to.equal('Blade Champion', 'Unit name');
+		expect(parsedList.units[0].points).to.equal(145, 'Unit points');
+		expect(parsedList.units[0].category).to.equal('CHARACTERS', 'Unit category');
+		expect(parsedList.units[0].models).to.have.lengthOf(1, 'Models');
+		expect(parsedList.units[0].models[0].name).to.equal('Blade Champion', 'Model name');
+		expect(parsedList.units[0].models[0].count).to.equal(1, 'Model count');
+		expect(parsedList.units[0].models[0].enhancements).to.have.lengthOf(1, 'Enhancements');
+		expect(parsedList.units[0].models[0].enhancements[0]).to.equal('Adamantine Talisman', 'Enhancement');
+		expect(parsedList.units[0].models[0].weapons).to.have.lengthOf(1, 'Weapons');
+		expect(parsedList.units[0].models[0].weapons[0]).to.equal('Vaultswords', 'Weapon');
+		expect(parsedList.units[0].models[0].weapons[0].count).to.equal(1, 'Weapon count');
+
+	});
+
 });
